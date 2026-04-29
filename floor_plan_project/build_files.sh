c70713e27f04017fc47ff19ad980db1e3eaa952b
rashad-alkhuja <show.stopper9090@gmail.com>
2026-04-29 23:19:00 +0400

    Fix: add --break-system-packages for Vercel uv-managed Python

--- floor_plan_project/build_files.sh
+++ floor_plan_project/build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install requirements
-pip install -r requirements.txt
+# Install requirements (--break-system-packages needed for Vercel's uv-managed Python)
+pip install --break-system-packages -r requirements.txt
 
 # Run collectstatic
 python manage.py collectstatic --noinput
